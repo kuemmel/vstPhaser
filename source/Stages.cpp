91f501cd832c2f21576c0c1c33ea1ad1a9d1e308
kuemmel <kuemmel@github.com>
2016-01-05 22:13:16 +0100

    stages class added // still problems with sdf........

---
+++ source/Stages.cpp
@@ -0,0 +1,69 @@
+#include "Stages.h"
+#include <stdexcept>
+#include <algorithm>
+#include <iterator>
+
+/**
+ * For documentation see stages.h
+ **/
+Stages::Stages() {
+	initArray();
+	init(0);
+}
+
+Stages::Stages(unsigned short stage_key) {
+	initArray();
+	init(stage_key);
+}
+
+void Stages::initArray() {
+	unsigned short stage_keys[5] = { 0, 2, 4, 8, 12 };
+	this->stage_keys = new unsigned short[5]();
+	for (int i = 0; i < 4; i++) {
+		this->stage_keys[i] = stage_keys[i];
+	}
+}
+
+Stages::Stages(unsigned short &custom_keys, unsigned short cutom_keys_length, unsigned short stage_key) {
+	this->stage_keys_length = cutom_keys_length;
+	this->stage_keys = &custom_keys;
+
+	init(stage_key);
+}
+
+void Stages::init(unsigned short stage_key) {
+	if (stage_key <= (this->stage_keys_length)) {
+		this->current_stage = stage_keys[stage_key];
+		this->index = 0;
+	}
+	else {
+		throw std::invalid_argument("stage_key out bounds(should be between 0 and 4)");
+	}
+}
+
+bool Stages::checkIndex() {
+	if (this->index < this->current_stage) {
+		this->index++;
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
+unsigned short Stages::getIndex() {
+	return this->index;
+}
+
+unsigned short Stages::reset() {
+	return this->resetIndex;
+}
+
+unsigned short Stages::resetIndex() {
+	this->index = 0;
+	return 0;
+}
+
+void Stages::setStage(unsigned short stage_key) {
+	this->current_stage = stage_keys[stage_key];
+}
---
+++ source/Stages.h
@@ -0,0 +1,77 @@
+#ifndef STAGES_H
+#define STAGES_H
+
+/**
+ * stages class a simple object oriented solution for returning the respective stage value
+ *	- a switch, structure makes more sense and is faster.
+ *
+ * returns the current index for a series of filters
+ * initialize with Stages stage = Stages(5); which is 12 stages, so stages.getIndex() will return the index and succ for values from 0 - 11
+ * 
+ **/
+class Stages {
+public:
+	Stages();
+	~Stages();
+
+	/**
+	 * Initialize the stages with a first key
+	 **/
+	Stages(unsigned short stage_key);
+
+	/**
+	 * Add custom keys for further development (a.i more filter-iterations/stages).
+	 **/
+	Stages(unsigned short &custom_keys, unsigned short cutom_keys_length, unsigned short stage_key);
+
+	/**
+	 * check wherever index is still inbound
+	 * So if the stage is 4 (meaning 12) and the index is 10 it will return true and add one).
+	 * Main ussage is inside a loop
+	 *  
+	 *  stages.setStage(2);
+	 *  while(stages.checkIndex()) {
+	 *     // should run 4 times
+	 *  }
+	 **/
+	bool checkIndex();
+
+	/**
+	* Returns the current index.
+	**/
+	unsigned short getIndex();
+
+	/**
+	 * short for resetIndex();
+	 **/
+	unsigned short reset();
+
+	/**
+	 * Resets the current index to 0 and returns 0.
+	 **/
+	unsigned short resetIndex();
+
+	/**
+	 * Sets the stage value according to the keys and resets the index.
+	 * default keys:
+	 * 0 ->  0 (off position, always return false)
+	 * 1 ->  2
+	 * 2 ->  4
+	 * 3 ->  8
+	 * 4 -> 12
+	 **/
+	void setStage(unsigned short stage_key);
+
+private:
+	//helper function, DRY
+	void init(unsigned short stage_key);
+	void initArray();
+
+	unsigned short current_stage;
+	unsigned short index;
+	//unsigned short stage_keys[5] = { 0, 2, 4, 8, 12 };
+	unsigned short * stage_keys;
+	unsigned short stage_keys_length = 5;
+};
+
+#endif
